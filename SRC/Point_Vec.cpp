59332c0dcccd9cad0baef0ff97199a8745aa8db3
MajidAbdelilah <abdelilahmaid012@gmail.com>
2024-08-26 21:24:16 +0100

    confirming it works

--- SRC/Point_Vec.cpp
+++ SRC/Point_Vec.cpp
@@ -1,9 +1,1 @@
-#include "Point_Vec.hpp"
 
-Vector operator-(Point a, Point b)
-{
-    Vector v;
-    v.x = a.x - b.x;
-    v.y = a.y - b.y;
-    return v;
-}
--- SRC/main.cpp
+++ SRC/main.cpp
@@ -3,15 +3,9 @@
 
 int main()
 {
-    Point p;
-    p.x = 0;
-    p.y = -1;
-    Point i;
-    i.x = 1;
-    i.y = 1;
-    Point c;
-    c.x = 2;
-    c.y = -1;
+    Vector p(0, -1);
+    Vector i(1, 1);
+    Vector c(2, -1);
 
     Vector ip = p - i;
     Vector cp = p - c;
@@ -19,6 +13,7 @@ int main()
     // std::cout << v.x << " " << v.y << std::endl;
     std::cout << ip.LengthSqrt() << "\n";
     std::cout << cp.LengthSqrt() << "\n";
-
+    std::cout << ip.x << ip.y << cp.x << cp.y << "\n";
+    std::cout << (ip + cp).x << (ip + cp).y << "\n";
     return 0;
 }
